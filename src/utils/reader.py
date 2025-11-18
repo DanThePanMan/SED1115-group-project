dec2bd594a1e0ae0b79df9cb22d0fc85f8179f90
DanThePanMan <danielchenschw@gmail.com>
2025-11-17 21:38:24 -0500

    feat: complete potentiometer class

--- src/utils/reader.py
+++ src/utils/reader.py
@@ -1,17 +1,47 @@
-def read_potentiometer_x():
-    """
-    Read the X-axis potentiometer value via ADC.
-    Input: None
-    Output: float (voltage between 0-3.3V)
-    Description: Reads ADC channel connected to X-axis pot and returns voltage.
-    """
-    pass
+class potentiometer():
+    
+    def __init__(self, pot_x, pot_y):
+        """
+        Constructor
+        Input: ADC pot_x, ADC pot_y
+        Output: None
+        Description: object contructor
+        """
+        self.pot_x = pot_x
+        self.pot_y = pot_y
+        
 
-def read_potentiometer_y():
-    """
-    Read the Y-axis potentiometer value via ADC.
-    Input: None
-    Output: float (voltage between 0-3.3V)
-    Description: Reads ADC channel connected to Y-axis pot and returns voltage.
-    """
-    pass
+    def map_range(value, input_low, input_high, output_low, output_high):
+        """
+        Map a value from one range to another.
+        Input: value, input_low, input_high, output_low, output_high
+        Output: float (mapped value)
+        Description: Takes a value from input range and maps it to output range.
+        """
+        return output_low + (output_high - output_low) * ((value - input_low) / (input_high - input_low))
+
+    def read_potentiometer_x(self):
+        """
+        Read the X-axis potentiometer value via ADC.
+        Input: None
+        Output: float (voltage between 0-3.3V)
+        Description: Reads ADC channel connected to X-axis pot and returns voltage.
+        """
+        raw_x = self.pot_x.read_u16()
+        x_mm = self.map_range(raw_x, 0, 65535, 30 , 240)
+
+
+        return x_mm
+
+    def read_potentiometer_y(self):
+        """
+        Read the Y-axis potentiometer value via ADC.
+        Input: None
+        Output: float (voltage between 0-3.3V)
+        Description: Reads ADC channel connected to Y-axis pot and returns voltage.
+        """
+        raw_y = self.pot_y.read_u16()
+        y_mm = self.map_range(raw_y, 0, 65535, 40 ,200)
+        
+        return y_mm
+
